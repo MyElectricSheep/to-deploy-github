ac3473753f27ae7da302125e43acc6d39e944ea1
Ben Paget <ben.paget@live.fr>
2020-10-15 13:39:49 +0100

    Updates

---
+++ precache-manifest.96b5d24b87f90c0a1e5533f9bafe027f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0324a91519a50b59971761199d2f42d",
+    "url": "/to-deploy-github/index.html"
+  },
+  {
+    "revision": "9164fa5b61aa46d5724d",
+    "url": "/to-deploy-github/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1cf69290be532e415aac",
+    "url": "/to-deploy-github/static/js/2.3c2f557d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/to-deploy-github/static/js/2.3c2f557d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9164fa5b61aa46d5724d",
+    "url": "/to-deploy-github/static/js/main.b183769b.chunk.js"
+  },
+  {
+    "revision": "ab8e640dc73e5a31a7e1",
+    "url": "/to-deploy-github/static/js/runtime-main.008b75ee.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/to-deploy-github/static/media/logo.5d5d9eef.svg"
+  }
+]);
